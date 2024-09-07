eff15bb9e4f5f62be2ef692eb98aaa5b4da72799
Jean-Francois Arbour <jf.arbour@gmail.com>
2024-09-07 16:40:48 -0400

    Tests for move generation

--- test.cpp
+++ test.cpp
@@ -1,7 +1,23 @@
 #include "catch2/catch_test_macros.hpp"
 #include "board.h"
+#include "movegen.h"
+#include <sstream>
 
 using namespace breakthrough;
+
+// Helper function to print Square
+inline std::string print(Square s) {
+    const char files[8] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
+    std::stringstream result;
+    result << files[s % 8];
+    result << int(s / 8) + 1;
+    return result.str();
+}
+
+// Operator overload for Move printing
+std::ostream& operator<<(std::ostream& out, const Move& move) {
+    return out << print(move.source) << print(move.target);
+}
 
 TEST_CASE("Piece functions", "[board]") {
     SECTION("is_empty function") {
@@ -23,7 +39,7 @@ TEST_CASE("Piece functions", "[board]") {
     }
 }
 
-TEST_CASE("Board default constructor initializes pieces correctly", "[Board]") {
+TEST_CASE("Board default constructor initializes pieces correctly", "[board]") {
     Board board;
 
     SECTION("First 16 squares are white pieces") {
@@ -31,13 +47,11 @@ TEST_CASE("Board default constructor initializes pieces correctly", "[Board]") {
             REQUIRE(board.at(Square(i)) == Piece::WHITE);
         }
     }
-
     SECTION("Middle 32 squares are empty") {
         for (int i = 16; i < 48; ++i) {
             REQUIRE(board.at(Square(i)) == Piece::EMPTY);
         }
     }
-
     SECTION("Last 16 squares are black pieces") {
         for (int i = 48; i < 64; ++i) {
             REQUIRE(board.at(Square(i)) == Piece::BLACK);
@@ -45,10 +59,58 @@ TEST_CASE("Board default constructor initializes pieces correctly", "[Board]") {
     }
 }
 
-TEST_CASE("Board outputs correct fen strings", "[Board]") {
+TEST_CASE("Board outputs correct fen strings", "[board]") {
     Board board;
 
     SECTION("Initial game has correct fen string") {
         REQUIRE(board.fen() == "PPPPPPPP/PPPPPPPP/////pppppppp/pppppppp w - - 0 1");
     }
+}
+
+TEST_CASE("MoveGen functionality", "[movegen]") {
+    Board board{};
+    MoveGen movegen;
+
+    SECTION("Initial board state") {
+        REQUIRE_FALSE(board.is_terminal());
+
+        auto moves = movegen.valid_moves(board);
+        REQUIRE(moves.size() > 0);
+        // Add more specific checks for the initial moves
+    }
+
+    SECTION("After first move") {
+        Move move{11, 20};
+        board.play(move);
+
+        REQUIRE_FALSE(board.is_terminal());
+
+        auto moves = movegen.valid_moves(board);
+        REQUIRE(moves.size() > 0);
+        // Add more specific checks for moves after the first move
+    }
+
+    SECTION("After second move") {
+        Move move1{11, 20};
+        Move move2{48, 40};
+        board.play(move1);
+        board.play(move2);
+
+        REQUIRE_FALSE(board.is_terminal());
+
+        auto moves = movegen.valid_moves(board);
+        REQUIRE(moves.size() > 0);
+        // Add more specific checks for moves after the second move
+    }
+}
+
+TEST_CASE("Move printing", "[move]") {
+    Move move1{11, 20};
+    Move move2{48, 40};
+    std::stringstream ss1;
+    std::stringstream ss2;
+    ss1 << move1;
+    ss2 << move2;
+    REQUIRE(ss1.str() == "d2e3");
+    REQUIRE(ss2.str() == "a7a6");
 }
